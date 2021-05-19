194de308c36b495047962a9310d105fb95fd6339
Artem <garribond85@gmail.com>
2021-05-19 16:21:07 +0500

    Added tables icons and transactions

--- 01_schema.sql
+++ 01_schema.sql
@@ -19,7 +19,28 @@ CREATE TABLE cards
     balance  BIGINT    NOT NULL DEFAULT 0,
     issuer   TEXT      NOT NULL CHECK ( issuer IN ('Visa', 'MasterCard', 'MIR') ),
     holder   TEXT      NOT NULL,
-    owner_id BIGINT    NOT NULL REFERENCES clients,
+    owner_id BIGINT    NOT NULL REFERENCES clients (id),
     status   TEXT      NOT NULL DEFAULT 'INACTIVE' CHECK (status IN ('INACTIVE', 'ACTIVE')),
     created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE icons
+(
+    id BIGINT PRIMARY KEY,
+    link TEXT NOT NULL
+);
+
+CREATE TABLE transactions
+(
+  id BIGSERIAL PRIMARY KEY,
+  amount BIGINT NOT NULL,
+  card_id BIGINT NOT NULL REFERENCES cards (id),
+  mcc_cod INT NOT NULL,
+  icon_id BIGINT NOT NULL REFERENCES icons (id) ,
+  status TEXT NOT NULL CHECK(status IN ('unsuccessfully', 'successfully')),
+  created  TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- DROP TABLE clients, cards, transactions, icons;
+
+-- DROP TABLE cards;
